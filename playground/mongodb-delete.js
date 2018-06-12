43fea8a20a09b55cbb17acc1d9230f0132c2b9c6
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-12 16:33:37 -0500

    mongodb-delete file add

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,7 @@
+const {MongoCient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err) {
+    console.log('Unable to connect to server...');
+  }
+});
