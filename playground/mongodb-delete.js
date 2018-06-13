c7fd6f5b11b1c091300de018f2701cf9f4574084
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-13 14:20:18 -0500

    delete data from table

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,7 +1,26 @@
-const {MongoCient, ObjectID} = require('mongodb');
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-  if(err) {
+  if (err) {
     console.log('Unable to connect to server...');
   }
+
+  console.log('Connected to server...')
+
+  let db = client.db('TodoApp');
+
+  // deleteOne
+
+  // deleteMany
+  db.collection('Users').deleteMany({name: 'Ilaha Cardoso'}).then((res) => {
+    console.log(res);
+  });
+
+  //findOneAndDelete
+
+
+
 });
