def8d5f1af5c5ce4c34029a39a09628b4f5a41a9
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-14 14:38:41 -0500

    add update files from database

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,15 +1,26 @@
-const {MongoClient, ObjectID} = require('mongodb');
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-  if(err) {
+  if (err) {
     console.log('unable to connect to server');
   }
 
-  console.log('Connected to server...');
+  console.log('Connected to MongoDB server...');
 
   let db = client('TodoApp');
 
-
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b22c29e3bcbebff9bfa69bb');
+  }, {
+    $set: {
+      name: 'Tiny Rick'
+    }
+  }, {
+    returnOriginal: false
+  });
 
   // client.close();
 });
