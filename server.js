09dec24b0ed680268cefa41683abbd20523a44ef
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-15 00:14:14 -0500

    start server.js file

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-let Todo = mongoose.model('Todo', {
+// todo model
+let Todo = mongoose.model('Todo', { // schema
   text: {
     type: String,
     required: true,
     minLength: 1,
-    trim
+    trim: true
   },
   complete: {
     type: Boolean,
@@ -20,23 +21,41 @@ let Todo = mongoose.model('Todo', {
   }
 });
 
-let newTodo = new Todo({
-  text: 'Learn how to program'
+// let newTodo = new Todo({
+//   text: 'Learn how to program'
+// });
+//
+// newTodo.save().then((res) => {
+//   console.log('Data saved to collection', res);
+// }, (err) => {
+//   console.log('Unable to persist data to collection');
+// });
+
+// let otherTodo = new Todo({
+//   text: 'this is a test with a validation schema',
+// });
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (err) => {
+//   console.log('Unable to persist data to collection');
+// });
+
+// user model
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  }
 });
 
-newTodo.save().then((res) => {
-  console.log('Data saved to collection', res);
-}, (err) => {
-  console.log('Unable to persist data to collection');
+let newUser = new User({
+  email: 'user@email'
 });
 
-let otherTodo = new Todo({
-  text: 'this is a test',
-  complete: true,
-  completedAt: 123
-});
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
+newUser.save().then((res) => {
+  console.log(JSON.stringify(res, undefined, 2));
 }, (err) => {
   console.log('Unable to persist data to collection');
 });
