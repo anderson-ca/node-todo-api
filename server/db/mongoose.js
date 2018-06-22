be0be34a1e51c313fc819e4c20e708ae5f7b7c44
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-22 15:38:05 -0500

    add URI connection to heroku db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const mongodbURI = 'mongodb://<dbuser>:<dbpassword>@ds163870.mlab.com:63870/c-137';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(mongodbURI || process.env.'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
