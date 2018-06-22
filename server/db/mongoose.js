51bb7a71afd89a6514c239ba75f5d59feb901cee
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-22 15:41:26 -0500

    fix URI link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongodbURI = 'mongodb://<dbuser>:<dbpassword>@ds163870.mlab.com:63870/c-137';
+const mongodbURI = 'mongodb://<anderson.d.cardoso>:<HPlovecraft92>@ds163870.mlab.com:63870/c-137';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodbURI || process.env.'mongodb://localhost:27017/TodoApp');
