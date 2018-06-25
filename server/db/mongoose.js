dbe8a39a9ed09111c70d01b17084c9a6fe2003b5
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-25 12:19:22 -0500

    set up app for heroku and deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongodbURI = 'mongodb://anderson.d.cardoso:HPlovecraft92@ds163870.mlab.com:63870/c-137';
+const mongodbURI = process.env.MONGODB_URI;
+console.log(mongodbURI);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodbURI || process.env.'mongodb://localhost:27017/TodoApp');
